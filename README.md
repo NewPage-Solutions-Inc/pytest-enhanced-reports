087252e9305c7df7af0571c114ce96d677e83def
mwzulfiqar <waleedspglobal@gmail.com>
2022-08-12 19:55:20 +0500

    1. video recording capability added 2. legacy code for levels of screenshot added 3. code to take screenshot on action chains perform() added 4. Readme file updated

--- README.md
+++ README.md
@@ -1,25 +1,84 @@
 # allure-screenshots
-QA innovation project to implement cypress style screenshots for allure reports.
+QA innovation project to implement cypress style screenshots and video recording for allure reports.
 
 Important Note:
 Download the Selenium Chrome from https://chromedriver.chromium.org/downloads. Make sure to unzip it and place the exe file on the root of the project. e.g. in this project, path of your chromedriver file could be "dev/chromedriver".
 Once you place the chrome file then use below commands to complete the setup.
 
-Install the Plugin:
-(Install the wheel dependencies mentioned in requirement.txt file first)
-1. Create a wheel file from source by running command ``python3 setup.py bdist_wheel`` inside `/screenshot_plugin` directory
-2. This command will create a wheel file in `/screenshot_plugin/dist` folder
-3. Install this package by running `pip install <path_to_wheel_file>`
+### Installing 'allure-screenshot'
 
 Helping commands to run tests on Windows/Mac, just install the package requirements and launch the test:
 1. pip install -r requirements\requirements.txt 
 2. pytest
+3. To install plugin remotely, below commands are useful depending on the repository access:
+if repo is public:
+pip install git+https://github.com/mrshahzadakram/allurescreenshotplugin.git/
+if repo is private, you may use your private token: 
+ pip install git+https://{PRIVATE_TOKEN}@github.com/mrshahzadakram/allurescreenshotplugin.git/
+or if you want to use ssh:
+pip install git+ssh://git@github.com/mrshahzadakram/allurescreenshotplugin.git
+
+### Usage
+
+#### Screenshots:
+Once plugin is installed then you may pass level of screenshots to be taken for allure reporting, e.g.
+- if you pass argument "screenshot_option" with value "all", then screenshot will be taken for each step in scenario: 
+ pytest -v -s -q dev/test/step_defs/ --report_screenshot_option all --report_image_resize_width 1720 --report_image_resize_height 1080  --alluredir=reports --driver Chrome --driver-path dev/chromedriver
+
+- if you pass argument "screenshot_option" with value "fail", then screenshot will be taken for only failed steps in scenario: 
+ pytest -v -s -q dev/test/step_defs/ --report_screenshot_option fail --report_image_resize_width 1720 --report_image_resize_height 1080  --alluredir=reports --driver Chrome --driver-path dev/chromedriver
+
+- you pass argument "screenshot_option" with value "none", then no screenshot will be shown to the allure report: 
+ pytest -v -s -q dev/test/step_defs/ --report_screenshot_option none --report_image_resize_width 1720 --report_image_resize_height 1080  --alluredir=reports --driver Chrome --driver-path dev/chromedriver
+
 - If you want to send image width and image height for the screenshot size in allure report then you can use "report_image_resize_width" and "report_image_resize_height" arguments with your desired values, and below command can be used as an example:
- `pytest -v -s -q dev/test/step_defs/  --report_image_resize_width 720 --report_image_resize_height 1080  --alluredir=reports --driver Chrome --driver-path dev/chromedriver`
-
+ pytest -v -s -q dev/test/step_defs/  --report_image_resize_width 720 --report_image_resize_height 1080  --alluredir=reports --driver Chrome --driver-path dev/chromedriver
 - If you want to reduce percentage size of the image to be taken for allure screenshot then you may send "report_image_resize_to_percent" with your desired value, and below is the helping command:
- ``pytest -v -s -q --report_image_resize_to_percent 50 dev/test/step_defs/ --alluredir=reports --driver Chrome --driver-path dev/chromedriver``                                 
+ pytest -v -s -q --report_image_resize_to_percent 50 dev/test/step_defs/ --alluredir=reports --driver Chrome --driver-path dev/chromedriver                                 
 
-3. After running any of above commands, you can check the Allure report (you may use any of below commands):
+#### Video-Recording:
+
+1. In pytest execution command, following video related flags can be used to enable video recording.
+2. Below-mentioned flags can be used to enable vide recording and set width and height.
+3. These flags can be used with flags of screenshots. Video flags will have no effect on the screenshot captures.
+
+##### Available Flags:
+
+| Flag                             | Expected Value | Description                                                                                                                                                                                                                                                                                                                                                                                                                                             |
+|----------------------------------|----------------|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| <b>--video_recording</b>         | true, false    | This flag is used to enable video recording. If value is not specified, it will default to <b>false</b>.                                                                                                                                                                                                                                                                                                                                                |
+| <b>--video_width</b>             | Number         | Used to set the width of the video frame.                                                                                                                                                                                                                                                                                                                                                                                                               |
+| <b>--video_height</b>            | Number         | Used to set the height of the video frame.                                                                                                                                                                                                                                                                                                                                                                                                              |
+| <b>--video_resize_percentage</b> | Number         | - Used to resize/reduce the height and width of original frames to a specific percentage.<br> - If video_width and video_height are given along with this flag then this flag will not have any effect.<br>- If video_width and height are not given but this flag is provided then frame will be resized to this percentage.<br>- If neither video_width, video_height nor video_resize_percentage is given then <b>this flag will default to 30%</b>. |
+| <b>--video_frame_rate</b>        | Number         | Used to set the number of frames per second in the video. <b>It will default to 5 fps</b> if not specified by user.                                                                                                                                                                                                                                                                                                                                     |
+
+##### Examples:
+1. Enable video recording with default settings:<br>``pytest -v -s -q dev/test/step_defs/ --alluredir=reports/runUpdatedVideoSingleThread --driver Chrome --driver-path /Users/mwaleedz/PycharmProjects/allure-screenshot/allure-screenshots/dev/chromedriver --video_recording true``<br>This command will generate video and by default reduce its frame width and height to 30%.
+2. Enable video recording with <b>video_width and video_height</b> flags:<br>``pytest -v -s -q dev/test/step_defs/ --alluredir=reports/runUpdatedVideoSingleThread --driver Chrome --driver-path /Users/mwaleedz/PycharmProjects/allure-screenshot/allure-screenshots/dev/chromedriver --video_recording true --video_width 1920 --video_height 1080``<br>This command will generate video with specified height and width.
+3. Enable video recording with <b>video_resize_percentage</b>:<br>``pytest -v -s -q dev/test/step_defs/ --alluredir=reports/runUpdatedVideoSingleThread --driver Chrome --driver-path /Users/mwaleedz/PycharmProjects/allure-screenshot/allure-screenshots/dev/chromedriver --video_recording true --video_resize_percentage 50``<br>This command will generate video and by default reduce its frame width and height to 50%.
+4. Enable video recording with <b>video_width, video_height, video_frame_rate</b> flags:<br>``pytest -v -s -q dev/test/step_defs/ --alluredir=reports/runUpdatedVideoSingleThread --driver Chrome --driver-path /Users/mwaleedz/PycharmProjects/allure-screenshot/allure-screenshots/dev/chromedriver --video_recording true --video_width 1920 --video_height 1080 --video_frame_rate 10``<br>This command will generate video with specified height and width with 10 frames per second.
+
+#### View Reports
+2. After running any of above commands, you can check the Allure report (you may use any of below commands):
 - allure serve reports
 - allure generate reports
+
+To use the Plugin locally to create screenshot use below commands:
+4. Create a wheel file from source by running command ``python3 setup.py bdist_wheel`` inside `/screenshot_plugin` directory
+- This command will create a wheel file in `/screenshot_plugin/dist` folder 
+- Install this package by running `pip install <path_to_wheel_file>`
+
+pytest -v -s -q dev/test/step_defs/ --report_screenshot_option all --report_image_resize_width 720 --report_image_resize_height 1080  --alluredir=reports --driver Chrome --driver-path dev/chromedriver
+
+
+--------------------------------------------------------------------------------------------------------
+Important note:
+Make sure your conftest file has fixture for web driver like below:
+
+@pytest.fixture
+def selenium(selenium, resize_info):
+    selenium.maximize_window()
+    driver = EventFiringWebDriver(selenium, WebDriverEventListener(resize_info))
+    yield driver
+    driver.quit()
+---------------------------------------------------------------------------------------------------------
--- allure_screenshot.py
+++ allure_screenshot.py
@@ -1,18 +1,38 @@
+
 """
 This module contains shared fixtures, steps, and hooks.
 """
 import base64
 import logging
 from typing import Tuple
-
 import allure
-from PIL import Image
 from io import BytesIO
 import pytest
 from allure_commons.types import AttachmentType
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
 import settings
+from selenium.webdriver.common.action_chains import ActionChains
+import wrapt.wrappers
+from PIL import Image
+import cv2
+import os
+import threading
+
+
+@wrapt.patch_function_wrapper(ActionChains, 'perform')
+def perform(wrapped, instance, args, kwargs):
+    # here, wrapped is the original perform,
+    # instance is `self` instance (it is not true for classmethods though),
+    # args and kwargs are tuple and dict respectively.
+    if pytest.image_info['screenshot_option'] == 'all':
+        path_to_resized_image = _get_resized_image(instance._driver.get_screenshot_as_base64(), pytest.image_info)
+        allure.attach.file(path_to_resized_image, name="Before ActionChain Perform", attachment_type=AttachmentType.PNG)
+        wrapped(*args, **kwargs)  # note it is already bound to the instance
+        path_to_resized_image = _get_resized_image(instance._driver.get_screenshot_as_base64(), pytest.image_info)
+        allure.attach.file(path_to_resized_image, name="Before ActionChain Perform", attachment_type=AttachmentType.PNG)
+    else:
+        wrapped(*args, **kwargs)
 
 
 @pytest.fixture(scope="session")
@@ -20,13 +40,25 @@ def resize_info(request) -> dict:
     cmd_line_resize_info: dict = {
         "resize_percent": request.config.getoption("report_image_resize_to_percent"),
         "resize_width": request.config.getoption("report_image_resize_width"),
-        "resize_height": request.config.getoption("report_image_resize_height")
+        "resize_height": request.config.getoption("report_image_resize_height"),
+        "screenshot_option": request.config.getoption("report_screenshot_option"),
+        "video_recording": request.config.getoption("video_recording"),
+        "video_width": request.config.getoption("video_width"),
+        "video_height": request.config.getoption("video_height"),
+        "video_frame_rate": request.config.getoption("video_frame_rate"),
+        "video_resize_percentage": request.config.getoption("video_resize_percentage")
     }
 
     env_resize_info: dict = {
         "resize_percent": settings.IMAGE_PERCENTAGE,
         "resize_width": settings.IMAGE_WIDTH,
-        "resize_height": settings.IMAGE_HEIGHT
+        "resize_height": settings.IMAGE_HEIGHT,
+        "screenshot_option": settings.SCREENSHOT_OPTION,
+        "video_recording": settings.VIDEO_RECORDING,
+        "video_width": settings.VIDEO_WIDTH,
+        "video_height": settings.VIDEO_HEIGHT,
+        "video_frame_rate": settings.VIDEO_FRAME_RATE,
+        "video_resize_percentage": settings.VIDEO_RESIZE_PERCENTAGE
     }
 
     """
@@ -42,6 +74,33 @@ def resize_info(request) -> dict:
     resize_percent = None
     resize_width = None
     resize_height = None
+    screenshot_option = None
+    video_recording = None
+    video_height = None
+    video_width = None
+    video_frame_rate = None
+    video_resize_percentage = None
+
+    if cmd_line_resize_info['screenshot_option']:
+        screenshot_option = cmd_line_resize_info['screenshot_option']
+    elif env_resize_info['screenshot_option']:
+        screenshot_option = env_resize_info['screenshot_option']
+
+    if cmd_line_resize_info['video_recording']:
+        video_recording = str(cmd_line_resize_info['video_recording']).lower() == 'true'
+    elif env_resize_info['video_recording']:
+        video_recording = str(env_resize_info['video_recording']).lower() == 'true'
+    else:
+        # If command line and env arguments are not given then set default video recording to False
+        video_recording = False
+
+    if cmd_line_resize_info['video_frame_rate']:
+        video_frame_rate = cmd_line_resize_info['video_frame_rate']
+    elif env_resize_info['video_frame_rate']:
+        video_frame_rate = env_resize_info['video_frame_rate']
+    else:
+        # If command line and env arguments are not given then set default video frame rate to '5'
+        video_frame_rate = 5
 
     if cmd_line_resize_info['resize_width'] and cmd_line_resize_info['resize_height']:
         resize_width = cmd_line_resize_info['resize_width']
@@ -55,10 +114,37 @@ def resize_info(request) -> dict:
         elif env_resize_info['resize_percent']:
             resize_percent = env_resize_info['resize_percent']
 
+    if cmd_line_resize_info['video_width'] and cmd_line_resize_info['video_height']:
+        video_width = cmd_line_resize_info['video_width']
+        video_height = cmd_line_resize_info['video_height']
+    elif env_resize_info['video_width'] and env_resize_info['video_height']:
+        video_width = env_resize_info['video_width']
+        video_height = env_resize_info['video_height']
+    else:
+        if cmd_line_resize_info['video_resize_percentage']:
+            video_resize_percentage = cmd_line_resize_info['video_resize_percentage']
+        elif env_resize_info['video_resize_percentage']:
+            video_resize_percentage = env_resize_info['video_resize_percentage']
+        else:
+            # If command line and env arguments are not given then set video Resize Factor to '30'
+            video_resize_percentage = 30
+
+    pytest.image_info = {
+        "resize_percent": resize_percent,
+        "resize_width": resize_width,
+        "resize_height": resize_height,
+        "screenshot_option": screenshot_option
+    }
     return {
         "resize_percent": resize_percent,
         "resize_width": resize_width,
-        "resize_height": resize_height
+        "resize_height": resize_height,
+        "screenshot_option": screenshot_option,
+        "video_recording": video_recording,
+        "video_height": video_height,
+        "video_width": video_width,
+        "video_frame_rate": video_frame_rate,
+        "video_resize_percentage": video_resize_percentage
     }
 
 
@@ -72,17 +158,64 @@ def logger():
     yield logger
 
 
+@pytest.fixture
+def screen_recorder():
+    obj = ScreenRecorder()
+    yield obj
+
+
+@pytest.fixture
+def video_capture_thread(screen_recorder, selenium):
+    recorder_thread = threading.Thread(target=screen_recorder.start_capturing, name='Recorder', args=[selenium])
+    yield recorder_thread, screen_recorder
+
+
 def pytest_addoption(parser):
     parser.addoption("--report_image_resize_to_percent", action="store", default=0)
     parser.addoption("--report_image_resize_width", action="store", default=0)
     parser.addoption("--report_image_resize_height", action="store", default=0)
+    parser.addoption("--report_screenshot_option", action="store", default="all")
+    parser.addoption("--video_recording", action="store", default=0)
+    parser.addoption("--video_width", action="store", default=0)
+    parser.addoption("--video_height", action="store", default=0)
+    parser.addoption("--video_frame_rate", action="store", default=0)
+    parser.addoption("--video_resize_percentage", action="store", default=0)
+
+
+def pytest_bdd_before_scenario(request, feature, scenario):
+    video_recording = request.getfixturevalue('resize_info')['video_recording']
+    if video_recording:
+        obj_recorder_thread, obj_recorder = request.getfixturevalue('video_capture_thread')
+        obj_recorder_thread.start()
+    logging.info("TEST EXECUTION VIDEO RECORDING: " + str(video_recording))
+
+
+def pytest_bdd_step_validation_error(request, feature, scenario, step, step_func):
+    driver = request.getfixturevalue('selenium')
+    level_value = request.getfixturevalue('resize_info')
+    if level_value['screenshot_option'] == 'fail' or level_value['screenshot_option'] == 'all':
+        path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), level_value)
+        allure.attach.file(path_to_resized_image, name="Step failed", attachment_type=AttachmentType.PNG)
 
 
 def pytest_bdd_step_error(request, feature, scenario, step, step_func):
     driver = request.getfixturevalue('selenium')
-    resize_info = request.getfixturevalue('resize_info')
-    path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), resize_info)
-    allure.attach.file(path_to_resized_image, name="Step failed", attachment_type=AttachmentType.PNG)
+    level_value = request.getfixturevalue('resize_info')
+    if level_value['screenshot_option'] == 'fail' or level_value['screenshot_option'] == 'all':
+        path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), level_value)
+        allure.attach.file(path_to_resized_image, name="Step failed", attachment_type=AttachmentType.PNG)
+
+
+def pytest_bdd_after_scenario(request, feature, scenario):
+    video_info = request.getfixturevalue('resize_info')
+    if video_info['video_recording']:
+        obj_recorder_thread, obj_recorder = request.getfixturevalue('video_capture_thread')
+        obj_recorder.stop = True
+        obj_recorder_thread.join()
+        video_resize_info = _get_video_resize_resolution(video_info)
+        obj_recorder.create_video_from_images(video_resize_info, video_info['video_frame_rate'])
+        allure.attach.file("video.webm", name=scenario.name, attachment_type=AttachmentType.WEBM)
+        logging.info("TEST EXECUTION VIDEO RECORDING VIDEO STOPPED [Video Size: " + str(video_resize_info) + " - Frame Rate: " + str(video_info['video_frame_rate']) + "]")
 
 
 class WebDriverEventListener(AbstractEventListener):
@@ -91,16 +224,65 @@ class WebDriverEventListener(AbstractEventListener):
         self.resize_info = resize_info
 
     def after_navigate_to(self, url, driver: WebDriver):
-        path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), self.resize_info)
-        allure.attach.file(path_to_resized_image, name="Navigation", attachment_type=AttachmentType.PNG)
+        if self.resize_info['screenshot_option'] == 'all':
+            path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), self.resize_info)
+            allure.attach.file(path_to_resized_image, name="Navigation", attachment_type=AttachmentType.PNG)
 
     def after_click(self, element, driver):
-        path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), self.resize_info)
-        allure.attach.file(path_to_resized_image, name="Click", attachment_type=AttachmentType.PNG)
+        if self.resize_info['screenshot_option'] == 'all':
+            path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), self.resize_info)
+            allure.attach.file(path_to_resized_image, name="Click", attachment_type=AttachmentType.PNG)
 
     def after_change_value_of(self, element, driver):
-        path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), self.resize_info)
-        allure.attach.file(path_to_resized_image, name="Keyboard input", attachment_type=AttachmentType.PNG)
+        if self.resize_info['screenshot_option'] == 'all':
+            path_to_resized_image = _get_resized_image(driver.get_screenshot_as_base64(), self.resize_info)
+            allure.attach.file(path_to_resized_image, name="Keyboard input", attachment_type=AttachmentType.PNG)
+
+
+class ScreenRecorder:
+
+    def __init__(self):
+        self.video = None
+        self.stop = False
+        self.directory = "video/"
+
+    def start_capturing(self, driver_):
+        """This method will start caotyring images and saving them on disk under /video folder
+            These images will later be used to stich together into a video"""
+        try:
+            count = 0
+            while True:
+                if not os.path.isdir(self.directory):
+                    os.mkdir(self.directory)
+                    logging.info("Creating new directory: " + self.directory)
+                driver_.save_screenshot("video/"+str(count)+".png")
+                count += 1
+                if self.stop:
+                    logging.info("Stopping Screen Capture")
+                    break
+            logging.error("SCREENSHOTS CAPTURED AND WRITTEN ON DISK: " + str(count))
+            print("SCREENSHOTS CAPTURED AND WRITTEN ON DISK: " + str(count))
+        except Exception as error:
+            logging.error("An Exception occurred while taking screenshot. " + str(error))
+
+    def create_video_from_images(self, video_size: tuple, frame_rate: int):
+        """This method will sticth the images under /video directory into a video"""
+        try:
+            fourcc = cv2.VideoWriter_fourcc(*'vp09')
+            video = cv2.VideoWriter("video.webm", fourcc, int(frame_rate), video_size)
+            img_dir = "video"
+            images_path = os.listdir(img_dir)
+            images_path = sorted(images_path, key=lambda x: int(os.path.splitext(x)[0]))
+            for img in images_path:
+                if img.__contains__('png'):
+                    video.write(cv2.resize(cv2.imread(img_dir + "/" + img), video_size))
+            video.release()
+            # Now clean the images directory
+            for f in images_path:
+                os.remove(os.path.join(img_dir, f))
+
+        except Exception as error:
+            logging.error("An Exception occurred while stitching video. " + str(error))
 
 
 def __get_resized_resolution(width, height, resize_factor) -> Tuple[int, int]:
@@ -109,17 +291,17 @@ def __get_resized_resolution(width, height, resize_factor) -> Tuple[int, int]:
     return new_width, new_height
 
 
-def _get_resized_image(image_bytes, resize_info):
+def _get_resized_image(image_bytes, info):
     desired_resolution = None
     # default resize factor if no values are passed from cmd line args or env vars
     resize_factor: float = 0.3
-    if resize_info:
-        if resize_info['resize_width'] and resize_info['resize_height']:
+    if info:
+        if info['resize_width'] and info['resize_height']:
             # if a resolution is provided, use that
-            desired_resolution = (int(resize_info['resize_width']), int(resize_info['resize_height']))
-        elif resize_info['resize_percent']:
+            desired_resolution = (int(info['resize_width']), int(info['resize_height']))
+        elif info['resize_percent']:
             # if a percentage is provided instead, some calculations are required
-            resize_factor = int(resize_info['resize_percent']) / 100
+            resize_factor = int(info['resize_percent']) / 100
 
     # open the image directly thru an in-memory buffer
     img = Image.open(BytesIO(base64.b64decode(image_bytes)))
@@ -135,3 +317,21 @@ def _get_resized_image(image_bytes, resize_info):
     path: str = "screenshot.png"
     img.save(path)
     return path
+
+
+def _get_video_resize_resolution(info):
+    desired_resolution = None
+    obj_screen_recorder = ScreenRecorder()
+    resize_factor = None
+    if info:
+        if info['video_width'] and info['video_height']:
+            # if a resolution is provided, use that
+            desired_resolution = (int(info['video_width']), int(info['video_height']))
+        elif info['video_resize_percentage']:
+            # if a percentage is provided, set the resize factor from default to user provided value
+            resize_factor = int(info['video_resize_percentage']) / 100
+            img = Image.open(os.path.join(obj_screen_recorder.directory, os.listdir(obj_screen_recorder.directory)[0]))
+            desired_resolution = __get_resized_resolution(img.width, img.height, resize_factor)
+
+    return desired_resolution
+
