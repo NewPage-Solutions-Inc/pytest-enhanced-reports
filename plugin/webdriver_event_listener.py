05919a33d91c9efff5ce089c6fe54395b72a7668
David <david.nguyen@newpage.io>
2022-09-15 21:11:23 +0700

    remove click if else

--- plugin/webdriver_event_listener.py
+++ plugin/webdriver_event_listener.py
@@ -19,10 +19,7 @@ class WebDriverEventListener(AbstractEventListener):
             highlight_element_and_take_a_screenshot(element, "Before click", self.screen_shot_plugin_options, driver)
 
     def after_click(self, element, driver):
-        if self.other_configs.get('highlight_element'):
-            _take_screenshot("After click", self.screen_shot_plugin_options, driver)
-        else:
-            _take_screenshot("Click", self.screen_shot_plugin_options, driver)
+        _take_screenshot("After click", self.screen_shot_plugin_options, driver)
         if self.other_configs.get('always_capture_log'):
             capture_output_and_attach_to_allure(driver)
 
@@ -32,10 +29,7 @@ class WebDriverEventListener(AbstractEventListener):
                                                     self.screen_shot_plugin_options, driver)
 
     def after_change_value_of(self, element, driver):
-        if self.other_configs.get('highlight_element'):
-            _take_screenshot("After keyboard input", self.screen_shot_plugin_options, driver)
-        else:
-            _take_screenshot("Keyboard input", self.screen_shot_plugin_options, driver)
+        _take_screenshot("After keyboard input", self.screen_shot_plugin_options, driver)
         if self.other_configs.get('always_capture_log'):
             capture_output_and_attach_to_allure(driver)
 
