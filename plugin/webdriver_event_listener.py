58496c390d087180b485865fb09aed3758ed57a6
mwzulfiqar <waleedspglobal@gmail.com>
2022-09-21 13:55:18 +0500

    Merged canvas version with latest enhanced-allure-screenshot version

--- plugin/webdriver_event_listener.py
+++ plugin/webdriver_event_listener.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
 from selenium.webdriver.remote.webdriver import WebDriver
-from allure_screenshot import _take_screenshot, highlight_element_and_take_a_screenshot
-from browser_output_manager import capture_output_and_attach_to_allure
+from allure_screenshot import take_screenshot, highlight_element_and_take_a_screenshot
+from browser_console_manager import capture_output
 
 
 class WebDriverEventListener(AbstractEventListener):
@@ -10,18 +10,18 @@ class WebDriverEventListener(AbstractEventListener):
         self.other_configs = other_configs
 
     def after_navigate_to(self, url, driver: WebDriver):
-        _take_screenshot(f"Navigation to {url}", self.screen_shot_plugin_options, driver)
+        take_screenshot(f"Navigation to {url}", self.screen_shot_plugin_options, driver)
         if self.other_configs.get('always_capture_log'):
-            capture_output_and_attach_to_allure(driver)
+            capture_output(driver)
 
     def before_click(self, element, driver):
         if self.other_configs.get('highlight_element'):
             highlight_element_and_take_a_screenshot(element, "Before click", self.screen_shot_plugin_options, driver)
 
     def after_click(self, element, driver):
-        _take_screenshot("After click", self.screen_shot_plugin_options, driver)
+        take_screenshot("After click", self.screen_shot_plugin_options, driver)
         if self.other_configs.get('always_capture_log'):
-            capture_output_and_attach_to_allure(driver)
+            capture_output(driver)
 
     def before_change_value_of(self, element, driver):
         if self.other_configs.get('highlight_element'):
@@ -29,15 +29,15 @@ class WebDriverEventListener(AbstractEventListener):
                                                     self.screen_shot_plugin_options, driver)
 
     def after_change_value_of(self, element, driver):
-        _take_screenshot("After keyboard input", self.screen_shot_plugin_options, driver)
+        take_screenshot("After keyboard input", self.screen_shot_plugin_options, driver)
         if self.other_configs.get('always_capture_log'):
-            capture_output_and_attach_to_allure(driver)
+            capture_output(driver)
 
     def after_execute_script(self, script, driver):
-        _take_screenshot("JS execution", self.screen_shot_plugin_options, driver)
+        take_screenshot("JS execution", self.screen_shot_plugin_options, driver)
         if self.other_configs.get('always_capture_log'):
-            capture_output_and_attach_to_allure(driver)
+            capture_output(driver)
 
     def after_navigate_back(self, driver):
         if self.other_configs.get('always_capture_log'):
-            capture_output_and_attach_to_allure(driver)
+            capture_output(driver)
