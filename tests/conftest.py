5576447bfe89c06a2c94543b22a9bb2e6625cc32
David <david.nguyen@newpage.io>
2022-11-08 22:37:07 +0700

    adding headless + chrome install

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,27 @@ import allure
 import pytest
 from pytest_bdd import when, parsers, then, given
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 logger = logging.getLogger(__name__)
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless", action="store", default="false", help="run test headless"
+    )
+
+
 @pytest.fixture
-def selenium(selenium, screenshotting_driver):
+def chrome_options(chrome_options, request):
+    headless = request.config.getoption("--headless")
+    if headless == "true":
+        chrome_options.add_argument("--headless")
+    return chrome_options
+
+
+@pytest.fixture
+def selenium(selenium, screenshotting_driver, request):
     enhanced_driver = None
     try:
         enhanced_driver = screenshotting_driver(selenium)
--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,8 @@
-echo '{"capabilities": {"browser": "Chrome","goog:loggingPrefs": {"browser": "ALL"}}}' > capabilities-js-test.json
+echo '{"capabilities": {"headless": true, "browser": "Chrome","goog:loggingPrefs": {"browser": "ALL"}}}' > capabilities-js-test.json
 python3.9 -m pytest --driver Chrome \
   --driver-path deb/chromedriver \
   --alluredir=reports \
+  --headless=true \
   --variables capabilities-js-test.json \
   --report_capture_browser_console_log 'always'
+
