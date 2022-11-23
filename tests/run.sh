4f4800396bb74bd52ce671f404737516d4e58c30
Swaraj Ramesh <swaraj.ramesh@pfizer.com>
2022-11-23 16:24:34 +0530

    Release/1.2.0 (#35)
    * Merged canvas version with latest enhanced-allure-screenshot version
    * reformatting files using black
    * integrate flake8
    * adding local app
    * adding tests
    * Decoupling reporting logic and other refactoring

---
+++ tests/run.sh
@@ -0,0 +1,7 @@
+echo '{"capabilities": {"headless": true, "browser": "Chrome","goog:loggingPrefs": {"browser": "ALL"}}}' > capabilities-js-test.json
+python3.9 -m pytest --driver Chrome \
+  --driver-path $CHROMEWEBDRIVER \
+  --alluredir=reports \
+  --variables capabilities-js-test.json \
+  --report_capture_browser_console_log 'always'
+
---
+++ tests/step_defs/test_js_logs.py
@@ -0,0 +1,54 @@
+from pytest_bdd import scenario
+import json
+import pytest
+from os import getcwd
+
+from .shared_steps import *  # noqa
+from tests.util import util
+
+
+@scenario("../features/test_site.feature", "Run Test for browser's outputs")
+def test_run_js_logs(driver):
+    pass
+
+
+@pytest.mark.order(after="test_run_js_logs")
+def test_verify_js_logs():
+    actual_file_dir = "reports"
+    actual_file = util.find_newest_report(
+        "Run Test for browser's outputs", actual_file_dir
+    )
+
+    # for a_file in list_of_files:
+    with open(actual_file) as f:
+        output = json.load(f)
+
+    if not output:
+        assert False, "Test was not run successfully or file not found!"
+
+    curr_dir = getcwd()
+    actual_files = []
+    for step in output["steps"]:
+        if step.get("attachments"):
+            for attachment in step.get("attachments"):
+                if "Logs from browser console" in attachment.get("name"):
+                    actual_files.append(
+                        f"{curr_dir}/{actual_file_dir}/"
+                        + attachment.get("source")
+                    )
+    expected_files = [
+        f"{curr_dir}/data/first_attachment.txt",
+        f"{curr_dir}/data/second_attachment.txt",
+    ]
+    assert len(actual_files) == len(
+        expected_files
+    ), "number of js output files are different"
+    for i in range(len(expected_files)):
+        with open(actual_files[i]) as act:
+            actual_file_content = "".join(act.readlines())
+        with open(expected_files[i]) as exp:
+            expected_file_content = exp.readlines()
+        for item in expected_file_content:
+            assert (
+                item.rstrip("\n") in actual_file_content
+            ), "console logs are not matching."
