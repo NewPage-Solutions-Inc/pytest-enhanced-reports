1c4e326499af3b852281901bd8816a5d5d3cf447
bharath-newpage <119424457+bharath-newpage@users.noreply.github.com>
2023-01-17 11:38:27 +0530

    improved names and added documentation

--- tests/step_defs/test_ss.py
+++ tests/step_defs/test_ss.py
@@ -31,6 +31,7 @@ def test_verify_ss():
     curr_dir = getcwd()
     actual_files = {}
     file_path = f"{curr_dir}/{actual_file_dir}/"
+    # Get screenshot attachments from the report attachment
     attachments = [
         attachment
         for step in output["steps"]
@@ -38,6 +39,7 @@ def test_verify_ss():
         for attachment in step.get("attachments")
         if "Screenshot" in attachment.get("name")
     ]
+    # Replace space with underscores of a source name for the comparison
     for attachment in attachments:
         if "Screenshot Navigation" in attachment.get("name"):
             actual_files["screenshot_navigation"] = file_path + attachment.get(
@@ -46,14 +48,21 @@ def test_verify_ss():
         else:
             key_name = attachment.get("name").replace(" ", "_")
             actual_files[key_name] = file_path + attachment.get("source")
-    for key, path in actual_files.items():
-        actual_image = cv2.imread(path)
-        h, w, e = actual_image.shape
+
+    # Iterate and compare the reports image with expected image
+    for image_name, image_path in actual_files.items():
+        actual_image = cv2.imread(image_path)
+
         expected_image = cv2.imread(
-            f"{curr_dir}/data/screenshots/{key}" + ".png"
+            f"{curr_dir}/data/screenshots/{image_name}" + ".png"
         )
+        height, width, e = actual_image.shape
         if actual_image.shape == expected_image.shape:
+            # Get the difference between the two images and returns an arrays with 0,1 and 2
+            # Mean Square Error (MSE) of the pixel values of the two images.
+            # Similar images will have less mean square error value.
+            # Using this method, we can compare two images having the same height, width and number of channels.
             difference = cv2.subtract(actual_image, expected_image)
             err = np.sum(difference**2)
-            mse = err / (h * w)
+            mse = err / (height * width)
             assert mse == 0.0 or mse <= 0.1
