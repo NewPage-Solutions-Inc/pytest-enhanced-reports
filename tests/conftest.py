09f80ca83f78d22d59caa495245ab3e4f0f32a48
David <david.nguyen@newpage.io>
2023-03-27 15:27:17 +0700

    add support firefox

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 logging.basicConfig(
     filename="reports/tests.log",
@@ -18,32 +18,44 @@ logger = logging.getLogger(__name__)
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--headless", action="store", default="true", help="run test headless"
+        "--headless", action="store", default="True", help="run test headless"
     )
 
 
 @pytest.fixture
 def old_driver(request):
-    chrome_options = webdriver.ChromeOptions()
-
-    headless = bool(request.config.getoption("--headless"))
-    if headless:
-        chrome_options.add_argument("--headless")
-
+    browser = request.config.getoption("--driver")
     caps = {"goog:loggingPrefs": {"browser": "ALL"}}
+    headless = request.config.getoption("--headless") == "True"
+    logger.error(browser)
+    if not browser or browser.lower() in ["chrome", "gc", "googlechrome"]:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--window-size=1024,768")
 
-    import os
+        if headless:
+            chrome_options.add_argument("--headless")
 
-    driver_path = os.getenv("CHROMEWEBDRIVER")
-    service = (
-        ChromeService(executable_path=driver_path)
-        if driver_path
-        else ChromeService(ChromeDriverManager().install())
-    )
+        import os
 
-    return webdriver.Chrome(
-        desired_capabilities=caps, options=chrome_options, service=service
-    )
+        driver_path = os.getenv("CHROMEWEBDRIVER")
+        service = (
+            ChromeService(executable_path=driver_path)
+            if driver_path
+            else ChromeService(ChromeDriverManager().install())
+        )
+
+        return webdriver.Chrome(
+            desired_capabilities=caps, options=chrome_options, service=service
+        )
+    elif browser.lower() in ["firefox", "ff"]:
+
+        options = FirefoxOptions()
+        if headless:
+            options.add_argument("--headless")
+
+        return webdriver.Firefox(desired_capabilities=caps, options=options)
+
+    assert False, f"browser {browser} isn't supported yet."
 
 
 @pytest.fixture
